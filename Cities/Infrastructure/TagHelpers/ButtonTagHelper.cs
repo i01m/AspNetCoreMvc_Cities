1b48e0c539e5719bd347786f94970d87e609fa6d
unknown <i.p.m.a.n.03.31.87@gmail.com>
2020-05-08 14:12:01 -0500

    First commit

---
+++ Cities/Infrastructure/TagHelpers/ButtonTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+
+namespace Cities.Infrastructure.TagHelpers
+{
+    //Narrowing the scope - Making it applied only to buttons which have bs-button-color attribute
+    //[HtmlTargetElement("button", Attributes="bs-button-color", ParentTag ="form")]
+
+    //Widening the scope - applies to any element which has attribute bs-button-color
+    //[HtmlTargetElement(Attributes = "bs-button-color", ParentTag = "form")]
+
+    //specifiying the complete set of elements
+    [HtmlTargetElement("button", Attributes = "bs-button-color", ParentTag = "form")]
+    [HtmlTargetElement("a", Attributes = "bs-button-color", ParentTag = "form")]
+    public class ButtonTagHelper : TagHelper
+    {
+        public string BsButtonColor { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("class", $"btn btn-{BsButtonColor}");
+        }
+    }
+}
---
+++ Cities/Models/City.cs
@@ -0,0 +1,10 @@
+﻿namespace Cities.Models
+{
+
+    public class City
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int? Population { get; set; }
+    }
+}
